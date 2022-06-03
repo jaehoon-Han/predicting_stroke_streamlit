60f556f9feca49572b95274ce537c263a1b0e124
jaehoon-Han <eroel1060@daum.net>
2022-06-03 10:58:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,59 +5,37 @@ import matplotlib as plt
 import seaborn as sns
 import os
 from PIL import Image
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
-import matplotlib.gridspec as grid_spec
-import seaborn as sns
-from imblearn.over_sampling import SMOTE
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler,LabelEncoder
-from sklearn.model_selection import train_test_split,cross_val_score
 
-
-from sklearn.linear_model import LinearRegression,LogisticRegression
-from sklearn.tree import DecisionTreeRegressor,DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-
-
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.metrics import accuracy_score, recall_score, roc_auc_score, precision_score, f1_score
 
 
 def main() :
     df = pd.read_csv('data/stroke.csv')
 
-    variables = [variable for variable in df.columns if variable not in ['id','stroke']]
+    add_selectbox = st.sidebar.selectbox(
+        'how would',
+        ('test','test2','test3')
+    )
 
-    conts = ['age','avg_glucose_level','bmi']
+    with st.sidebar :
+        col1, col2 = st.columns(2)
+        with col1 : st.button('button test 1')
+        with col2 : st.button('button test 2')
 
-    fig = plt.figure(figsize=(12, 12), dpi=150, facecolor='#fafafa')
-    gs = fig.add_gridspec(4, 3)
-    gs.update(wspace=0.1, hspace=0.4)
 
-    background_color = "#fafafa"
+    with st.sidebar :
+        add_radio = st.radio(
+            'test text',
+            ('radio test1', 'radio test2')
+        )
+        st.slider('slider test',10,100)
 
-    plot = 0
-    for row in range(0, 1):
-        for col in range(0, 3):
-            locals()["ax"+str(plot)] = fig.add_subplot(gs[row, col])
-            locals()["ax"+str(plot)].set_facecolor(background_color)
-            locals()["ax"+str(plot)].tick_params(axis='y', left=False)
-            locals()["ax"+str(plot)].get_yaxis().set_visible(False)
-            for s in ["top","right","left"]:
-                locals()["ax"+str(plot)].spines[s].set_visible(False)
-            plot += 1
+        st.checkbox('test checkbox',value= False)
 
-    plot = 0
-    for variable in conts:
-            sns.kdeplot(df[variable] ,ax=locals()["ax"+str(plot)], color='#0f4c81', shade=True, linewidth=1.5, ec='black',alpha=0.9, zorder=3, legend=False)
-            locals()["ax"+str(plot)].grid(which='major', axis='x', zorder=0, color='gray', linestyle=':', dashes=(1,5))
-            #locals()["ax"+str(plot)].set_xlabel(variable) removed this for aesthetics
-            plot += 1
-            
-
-    plt.show()
+    with st.sidebar :
+        st.text('''17.3 Copyright @ 2022
+Delta Dental of NJ and CT, Inc.''')
+        
+        
 
 if __name__ == '__main__' :
     main()
