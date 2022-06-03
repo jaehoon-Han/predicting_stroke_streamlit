165104533f76495cdfec42c65334cedde4f4a866
jaehoon-Han <eroel1060@daum.net>
2022-06-03 09:31:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import pandas as pd
+import streamlit as st
 import numpy as np
 import matplotlib as plt
 import seaborn as sns
 import os
-from PIL import Ima
+from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 import matplotlib.gridspec as grid_spec
@@ -23,42 +24,40 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import accuracy_score, recall_score, roc_auc_score, precision_score, f1_score
 
-df = pd.read_csv('data/stroke.csv')
 
-variables = [variable for variable in df.columns if variable not in ['id','stroke']]
+def main() :
+    df = pd.read_csv('data/stroke.csv')
 
-conts = ['age','avg_glucose_level','bmi']
+    variables = [variable for variable in df.columns if variable not in ['id','stroke']]
 
-fig = plt.figure(figsize=(12, 12), dpi=150, facecolor='#fafafa')
-gs = fig.add_gridspec(4, 3)
-gs.update(wspace=0.1, hspace=0.4)
+    conts = ['age','avg_glucose_level','bmi']
 
-background_color = "#fafafa"
+    fig = plt.figure(figsize=(12, 12), dpi=150, facecolor='#fafafa')
+    gs = fig.add_gridspec(4, 3)
+    gs.update(wspace=0.1, hspace=0.4)
 
-plot = 0
-for row in range(0, 1):
-    for col in range(0, 3):
-        locals()["ax"+str(plot)] = fig.add_subplot(gs[row, col])
-        locals()["ax"+str(plot)].set_facecolor(background_color)
-        locals()["ax"+str(plot)].tick_params(axis='y', left=False)
-        locals()["ax"+str(plot)].get_yaxis().set_visible(False)
-        for s in ["top","right","left"]:
-            locals()["ax"+str(plot)].spines[s].set_visible(False)
-        plot += 1
+    background_color = "#fafafa"
 
-plot = 0
-for variable in conts:
-        sns.kdeplot(df[variable] ,ax=locals()["ax"+str(plot)], color='#0f4c81', shade=True, linewidth=1.5, ec='black',alpha=0.9, zorder=3, legend=False)
-        locals()["ax"+str(plot)].grid(which='major', axis='x', zorder=0, color='gray', linestyle=':', dashes=(1,5))
-        #locals()["ax"+str(plot)].set_xlabel(variable) removed this for aesthetics
-        plot += 1
-        
-ax0.set_xlabel('Age')
-ax1.set_xlabel('Avg. Glucose Levels')
-ax2.set_xlabel('BMI')
+    plot = 0
+    for row in range(0, 1):
+        for col in range(0, 3):
+            locals()["ax"+str(plot)] = fig.add_subplot(gs[row, col])
+            locals()["ax"+str(plot)].set_facecolor(background_color)
+            locals()["ax"+str(plot)].tick_params(axis='y', left=False)
+            locals()["ax"+str(plot)].get_yaxis().set_visible(False)
+            for s in ["top","right","left"]:
+                locals()["ax"+str(plot)].spines[s].set_visible(False)
+            plot += 1
 
+    plot = 0
+    for variable in conts:
+            sns.kdeplot(df[variable] ,ax=locals()["ax"+str(plot)], color='#0f4c81', shade=True, linewidth=1.5, ec='black',alpha=0.9, zorder=3, legend=False)
+            locals()["ax"+str(plot)].grid(which='major', axis='x', zorder=0, color='gray', linestyle=':', dashes=(1,5))
+            #locals()["ax"+str(plot)].set_xlabel(variable) removed this for aesthetics
+            plot += 1
+            
 
-ax0.text(-20, 0.022, 'Numeric Variable Distribution', fontsize=20, fontweight='bold', fontfamily='serif')
-ax0.text(-20, 0.02, 'We see a positive skew in BMI and Glucose Level', fontsize=13, fontweight='light', fontfamily='serif')
+    plt.show()
 
-plt.show()
+if __name__ == '__main__' :
+    main()
